b3a0b35d48e2c9ca502a6df446fba48dadb24872
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-08-31 07:34:37 -0300

    Update criarTabelas.js

--- api/banco-de-dados/criarTabelas.js
+++ api/banco-de-dados/criarTabelas.js
@@ -1,5 +1,5 @@
 const ModeloTabela = require('../rotas/fornecedores/ModeloTabelaFornecedor')
-
+//chamando a classe que cria a tabela com Promise
 ModeloTabela
     .sync()
     .then(() => console.log('Tabela criada com sucesso'))
